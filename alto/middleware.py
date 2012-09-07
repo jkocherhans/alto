59d1bdd0c96ace106d756276f5289a636a6f983c
Joseph Kocherhans <joseph@jkocherhans.com>
2012-09-07 06:52:55 -0500

    Only enable alto if DEBUG=True.

--- alto/middleware.py
+++ alto/middleware.py
@@ -3,9 +3,5 @@ from django.utils.importlib import import_module
 
 class AltoMiddleware(object):
     def process_request(self, request):
-        # # Import the existing urlpatterns and add alto's patterns to it.
-        # urlconf_module = import_module(settings.ROOT_URLCONF)
-        # existing_urlpatterns = getattr(urlconf_module, 'urlpatterns')
-        # urlpatterns = [url(r'^_alto/', include('alto.urls'))] + existing_urlpatterns
-        # print request.urlconf
-        request.urlconf = 'alto.utils.url_overrides'
+        if settings.DEBUG:
+            request.urlconf = 'alto.utils.url_overrides'
